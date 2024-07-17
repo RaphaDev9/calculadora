4451f872796e0d1ebab04a03e5aaf2e5bfffe252
RaphaDev9 <raphael.miranda1998@gmail.com>
2024-07-17 20:39:31 -0300

    ajuste no botão reset, historico, operadores, atualização de display e salvar operador

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-// Selecione os números
+// selecione os números
+
+let inputDisplay = document.getElementById("display");
 
 function selecao(e){
     let valor = '';
@@ -11,21 +13,20 @@ numeros.forEach(num => {
     num.addEventListener("click", selecao);
 })
 
-/* Guardar número anterior e inserir no display */
+/* guardar número anterior e inserir no display */
 
 let numerais = '';
 
 function numAnterior(num){
 
     if (num === ",") {
-        // Se já houver um ponto decimal na string numerais, não adiciona outro
+        // se já houver um ponto decimal na string numerais, não adiciona outro
         if (numerais.includes(",")) {
             return;
         }
     }
 
     numerais = numerais + num
-    let inputDisplay = document.getElementById("display");
     inputDisplay.value = numerais;
 }
 
@@ -33,6 +34,50 @@ function numAnterior(num){
 
 function reset(){
     numerais = ''
-    let inputDisplay = document.getElementById("display");
+    inputDisplay.value = '';
+    let historico = document.querySelector("#historico");
+    historico.innerHTML = '';
+}
+
+/* limpar display */
+
+function limpaDisplay(){
+    numerais = ''
     inputDisplay.value = '';
 }
+
+/* operações */
+
+let operacao = '';
+
+/* salvar operador */
+
+const op = document.querySelectorAll(".operador");
+op.forEach(op => {
+    op.addEventListener("click", atualizarDisplay);
+})
+
+/* atualizar display */
+
+function atualizarDisplay(o){
+    let op = '';
+    op = o.target.textContent;
+    operacao = operacao + numerais + op;
+    limpaDisplay();
+    atualizarHistorico(operacao);
+    console.log(operacao);
+}
+
+/* atualizar historico */
+
+function atualizarHistorico(his){
+    let historico = document.querySelector("#historico");
+    historico.innerHTML = his;
+}
+
+/* resultado */
+
+function resultado(){
+    
+}
+
