04a57f6e53e6a1cbde5964e5b437451079958f22
RaphaDev9 <raphael.miranda1998@gmail.com>
2024-07-17 18:16:42 -0300

    Botão de Reset e salvar numerais digitados sequencialmente

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
-// Selecione um número e adicione ao input
+// Selecione os números
 
 function selecao(e){
     let valor = '';
-    let inputDisplay = document.getElementById("display");
     valor = e.target.textContent;
-    console.log(valor);
-    inputDisplay.value = valor;
+    if(valor == ","){
+        console.log("virgula")
+        valor = ".";
+    }
+
+    numAnterior(valor);
 }
 
-const numeros = document.querySelectorAll(".num");
+const numeros = document.querySelectorAll(".num, .virgula");
 numeros.forEach(num => {
     num.addEventListener("click", selecao);
 })
 
-/**/
+/* Guardar número anterior e inserir no display */
+
+let numerais = '';
+
+function numAnterior(num){
+    numerais = numerais + num
+    let inputDisplay = document.getElementById("display");
+    inputDisplay.value = numerais;
+}
+
+/* botão reset */
+
+function reset(){
+    numerais = ''
+    let inputDisplay = document.getElementById("display");
+    inputDisplay.value = '';
+}
